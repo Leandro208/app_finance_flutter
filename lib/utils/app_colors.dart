d59408bc9e69f7d7162f7bb0bc7cd70b82100a88
arthur <arthur.beserra@esig.com.br>
2024-04-09 08:12:44 -0300

    cores

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -5,5 +5,7 @@ class AppColors {
   static const Color secondaryColor = Color.fromARGB(255, 26, 35, 126);
   static const Color despesaColor = Colors.red;
   static const Color receitaColor = Colors.green;
+  static const Color background = Color(0xff15141C);
+  static const Color onBackground = Color(0xff2C2A38);
+  static const Color purple = Color.fromARGB(255, 40, 1, 63);
 }
-
