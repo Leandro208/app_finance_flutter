c136f72a13b7cca6bfd2ddb3b26a13b6f181e7a9
arthur <arthur.beserra@esig.com.br>
2024-04-09 08:11:44 -0300

    adiciona novos campos

--- lib/model/movimentacao.dart
+++ lib/model/movimentacao.dart
@@ -1,10 +1,20 @@
+import 'dart:math';
+
 import 'tipo_movimentacao.dart';
 
 class Movimentacao {
-  String descricao;
+  final String id;
+  String titulo;
   double valor;
   DateTime data;
+  String? descricao;
   TipoMovimentacao tipo;
 
-  Movimentacao({required this.descricao, required this.valor, required this.data, required this.tipo});
+  Movimentacao({
+    required this.titulo,
+    required this.valor,
+    required this.data,
+    required this.tipo,
+    this.descricao,
+  }) : id = Random().nextInt(9999).toString();
 }
